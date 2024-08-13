39ac42081c0f26310da61ffe9831ed2535b35ee4
saitou78 <L2404826526as@gmail.com>
2024-08-13 14:25:59 +0900

    初回コミット

---
+++ src/main/java/raisetech/SutudentManagement/SutudentManagementApplication.java
@@ -0,0 +1,13 @@
+package raisetech.SutudentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SutudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SutudentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/raisetech/SutudentManagement/SutudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.SutudentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SutudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
