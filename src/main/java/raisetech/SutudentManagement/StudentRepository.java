ad34441ef3588ddb167bf2dba41107f1b915b044
saitou78 <L2404826526as@gmail.com>
2024-08-22 16:13:57 +0900

    初回コミット

---
+++ src/main/java/raisetech/SutudentManagement/StudentRepository.java
@@ -0,0 +1,24 @@
+package raisetech.SutudentManagement;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM student")
+  List<Student> getAllStudent();
+
+  @Insert("INSERT student values(#{name}, #{age})")
+  void registerStudent(String name, int age);
+
+  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
+  void updateStudent(String name, int age);
+
+  @Delete("DELETE FROM student WHERE name = #{name}")
+  void deleteStudent(String name);
+}
--- src/test/java/raisetech/SutudentManagement/StudentManagementApplicationTests.java
+++ src/test/java/raisetech/SutudentManagement/StudentManagementApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SutudentManagementApplicationTests {
+class StudentManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
